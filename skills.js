6c0f2cbf42493a45eb9bd58a37231b34e8fb1968
Svetlana <Kira.0361@yandex.ru>
2021-06-24 01:49:50 +0300

    Присутствуют навыки из JS

---
+++ skills.js
@@ -0,0 +1,36 @@
+const skills = [
+    {css_class: "skill-java", name: "Java", level: 78},
+    {css_class: "skill-c-plus", name: "C++", level: 75},
+    {css_class: "skill-css", name: "Cascading Style Sheets", level: 44},
+    {css_class: "skill-html", name: "Hyper Text Markup Language", level: 32},
+    {css_class: "skill-php", name: "Hypertext Preprocessor", level: 26}
+]
+
+
+
+
+function add_skills() { 
+    const dl = document.createElement('dl');
+    dl.classList.add('skills-list');
+    skills.forEach(item => {
+
+        const dt = document.createElement('dt');
+        dt.textContent = item.name;
+        dt.classList.add(item.css_class);
+
+        const dd = document.createElement('dd');
+        dd.classList.add('level');
+
+        const div = document.createElement('div');
+        div.style.width = item.level + '%';
+        div.textContent = item.level;
+
+        dd.appendChild(div);
+        dl.append(dt);
+        dl.append(dd);
+    });
+
+    document.getElementById('skills').append(dl)
+}
+
+add_skills();
